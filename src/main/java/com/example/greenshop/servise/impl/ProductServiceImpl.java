b878571162cd85334bbfcc80729adcfb122a9d2d
Comp <ovojan84@gmail.com>
2023-06-08 21:06:40 +0400

    add repository

---
+++ src/main/java/com/example/greenshop/servise/impl/ProductServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.greenshop.servise.impl;
+
+import com.example.greenshop.entity.Product;
+import com.example.greenshop.entity.User;
+import com.example.greenshop.repository.ProductRepository;
+import com.example.greenshop.servise.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+
+    @Value("${greenshop.upload.image.path}")
+    private String imageUploadPath;
+
+    private final ProductRepository productRepository;
+
+    @Override
+    public List<Product> findProducts() {
+        List<Product> products =productRepository.findAll();
+        return products;
+    }
+    @Override
+    public Optional<Product> findById(int id) {
+        return productRepository.findById(id);
+    }
+    @Override
+    public void addProduct(User currentUser, MultipartFile multipartFile, Product product) throws IOException {
+        if (multipartFile != null && !multipartFile.isEmpty()) {
+            String fileName = System.nanoTime() + "_" + multipartFile.getOriginalFilename();
+            File file = new File(imageUploadPath + fileName);
+            multipartFile.transferTo(file);
+            product.setImage(fileName);
+        }
+        productRepository.save(product);
+    }
+    @Override
+    public void deleteById(int id) {
+        productRepository.deleteById(id);
+    }
+}
